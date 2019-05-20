e63573469625bdbaa8de6f13b94f599590c78e80
oliviawojcik <olivia.wojcik@hotmail.com>
2019-05-20 18:25:55 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>My Website</title>
+	<meta name="description" content="It's a test page.">
+    <link rel="icon" href="img/fav.png" type="image/x-icon">
+
+    <!-- main css -->
+    <link href="style.css" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+
+    <!-- fancyBox -->
+<link rel="stylesheet" href="/fancybox/source/jquery.fancybox.css?v=2.1.7" type="text/css" media="screen" />
+<script type="text/javascript" src="/fancybox/source/jquery.fancybox.pack.js?v=2.1.7"></script>
+
+</head>
+
+<body>
+	<nav>
+	<ul>
+		<li><a href="">Home</a></li>
+		<li><a href="">About us</a></li>
+		<li><a href="">Where to find us</a></li>
+		<li><a href="">Link Test</a></li>
+		<li><a href="">Link Test</a></li>
+		<li><a href="">Link Test</a></li>
+		<li><a href="">Link Test</a></li>
+		<li><a href="">Link Test</a></li>
+		<li><a href="">Contact us</a></li>
+		</ul>
+	</nav>
+	<header>
+	</header>
+<main>
+	<section id="welcome-content">
+	<h1>Welcome</h1>
+		<article><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
+			<p>When an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. <a href="/contact">Contact us today.</a></p></article>
+	<aside>
+		<h3>Why choose us?</h3>
+		<ul>
+		<li>Free quotations</li>
+			<li>Expert Staff</li>
+			<li>Years of industry knowledge</li>
+			<li>80+ talented members of staff</li>
+		</ul>
+		</aside>
+	
+	</section>
+
+	
+	
+	</main>
+	<footer>
+	<p>&#169;2015 My Website Ltd.</p>
+	
+	</footer>
+ 
+</body>
+
+</html>
